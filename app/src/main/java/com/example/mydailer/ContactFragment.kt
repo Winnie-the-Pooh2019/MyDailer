f135a9e55441c2ccc485020836e61bf8ae24e406
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2022-05-05 00:23:50 +0700

    fragment works

--- app/src/main/java/com/example/mydailer/ContactFragment.kt
+++ app/src/main/java/com/example/mydailer/ContactFragment.kt
@@ -1,10 +1,27 @@
 package com.example.mydailer
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mydailer.adapter.DayAdapter
+import com.example.mydailer.domain.objects.Phone
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+import java.net.HttpURLConnection
+import java.net.URL
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +37,59 @@ class ContactFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    
+    private val currentContext by lazy { 
+        activity!!
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val phoneList: Array<Phone> =
+                try {
+                    Gson().fromJson(downloadJson(), Array<Phone>::class.java)
+                } catch (e: Exception) {
+                    Timber.e(e)
+                    emptyArray()
+                }
+
+            val recyclerView = currentContext.findViewById<RecyclerView>(R.id.recycler_view).apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = DayAdapter(phoneList.toList())
+            }
+
+            val search = currentContext.findViewById<EditText>(R.id.text_search)
+            search.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    val adapter = recyclerView.adapter!! as DayAdapter
+                    adapter.phoneList = adapter.original.filter { it.searchPhone() }
+                    adapter.notifyDataSetChanged()
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    if (p0 == null) {
+                        Timber.e("no edit text found")
+                        return
+                    }
+
+                    val sharedPreferences = currentContext.getPreferences(Context.MODE_PRIVATE)
+                    with(sharedPreferences.edit()) {
+                        Timber.w(p0.toString())
+                        putString(getString(R.string.preferences_key), p0.toString())
+                        apply()
+                    }
+                }
+            })
+            search.setText(currentContext.getPreferences(Context.MODE_PRIVATE).let {
+                return@let it.getString(this@ContactFragment.getString(R.string.preferences_key), "")!!
+            })
         }
     }
 
@@ -34,7 +98,25 @@ class ContactFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
+
+    private fun Phone.searchPhone(): Boolean {
+        val searchString = currentContext.findViewById<TextView>(R.id.text_search).text
+
+        return this.phone.contains(searchString) || this.name.contains(searchString)
+                || this.type.contains(searchString) || searchString.isEmpty()
+    }
+
+    private suspend fun downloadJson(): String = withContext(Dispatchers.IO) {
+        val connection = URL(getString(R.string.download_link))
+            .openConnection() as HttpURLConnection
+        try {
+            connection.inputStream.bufferedReader().readText()
+        } finally {
+            connection.disconnect()
+        }
     }
 
     companion object {
@@ -48,7 +130,7 @@ class ContactFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String? = null, param2: String? = null) =
             ContactFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
--- app/src/main/java/com/example/mydailer/MainActivity.kt
+++ app/src/main/java/com/example/mydailer/MainActivity.kt
@@ -1,21 +1,8 @@
 package com.example.mydailer
 
-import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.mydailer.adapter.DayAdapter
-import com.example.mydailer.domain.objects.Phone
-import com.google.gson.Gson
-import kotlinx.coroutines.*
 import timber.log.Timber
-import java.net.HttpURLConnection
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,64 +11,11 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        CoroutineScope(Dispatchers.Main).launch {
-            val phoneList: Array<Phone> =
-                try {
-                    Gson().fromJson(downloadJson(), Array<Phone>::class.java)
-                } catch (e: Exception) {
-                    Timber.e(e)
-                    emptyArray()
-                }
-
-            val recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
-                layoutManager = LinearLayoutManager(context)
-                adapter = DayAdapter(phoneList.toList())
-            }
-
-            val search = findViewById<EditText>(R.id.text_search)
-            search.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    val adapter = recyclerView.adapter!! as DayAdapter
-                    adapter.phoneList = adapter.original.filter { it.searchPhone() }
-                    adapter.notifyDataSetChanged()
-                }
-
-                override fun afterTextChanged(p0: Editable?) {
-                    if (p0 == null) {
-                        Timber.e("no edit text found")
-                        return
-                    }
-
-                    val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
-                    with(sharedPreferences.edit()) {
-                        Timber.w(p0.toString())
-                        putString(getString(R.string.preferences_key), p0.toString())
-                        apply()
-                    }
-                }
-            })
-            search.setText(getPreferences(Context.MODE_PRIVATE).let {
-                return@let it.getString(this@MainActivity.getString(R.string.preferences_key), "")!!
-            })
-        }
-    }
-
-    private fun Phone.searchPhone(): Boolean {
-        val searchString = findViewById<TextView>(R.id.text_search).text
-
-        return this.phone.contains(searchString) || this.name.contains(searchString)
-                || this.type.contains(searchString) || searchString.isEmpty()
-    }
-
-    private suspend fun downloadJson(): String = withContext(Dispatchers.IO) {
-        val connection = URL(getString(R.string.download_link))
-            .openConnection() as HttpURLConnection
-        try {
-            connection.inputStream.bufferedReader().readText()
-        } finally {
-            connection.disconnect()
+        // зачем это надо, если мы в лайауте устанаваливаем класс фрагмента и все хорошо воркает -_-
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_fragment, ContactFragment.newInstance(), null)
+                .commit()
         }
     }
 }
